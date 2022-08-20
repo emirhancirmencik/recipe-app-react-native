b7b3b9466ef25b470791487fea922a27063c4a50
Emirhan ÇİRMENCİK <emrhncrmnck1905@gmail.com>
2022-08-20 06:11:03 +0300

    Navigation and config init.

---
+++ components/Categories/Categories.js
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import Styles from "./Categories.styles";
+import React from "react";
+import Config from "react-native-config";
+
+const Categories = () => {
+  return (
+    <View>
+      <Text>Categories</Text>
+    </View>
+  );
+};
+
+export default Categories;
